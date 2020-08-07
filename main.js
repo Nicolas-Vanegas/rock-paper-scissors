6ab6a28a2f1856e785e140e6a97bb45c99cf9f6a
Nicolás Vanegas <Nicolas.vanegas.p@hotmail.com>
2020-08-07 11:28:45 -0500

    Now the game alllow you enter your choice by typing, show reslut in console

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
 //Problema: Juego de piedra papel o tijera, darle una de las 3 elecciones al usuario y a la maquina y decirles quien ganó.
 
-var userSelect;
+var userSelect = prompt("Enter your choice between rock, papers and scissors");
 var machineSelect;
+var mensaje;
 
+//darle un arma aleatoria a la maquina
 var machineSelectNumber = Math.round(Math.random()* 3);
 if(machineSelectNumber == 1) {
-  var machineSelect = "Rock";
+  var machineSelect = "rock";
 }else if (machineSelectNumber == 2) {
-  var machineSelect = "Paper";
+  var machineSelect = "paper";
 }else{
-  var machineSelect = "Scissors";
+  var machineSelect = "scissors";
 }
 
-const resultado = function(userSelect, machineSelect){ 
+console.log(machineSelect); 
+
+
   if(userSelect != machineSelect) {
     if(userSelect == "rock" && machineSelect == "scissors" || userSelect == "scissors" && machineSelect == "paper" || userSelect == "paper" && machineSelect == "rock"){
-      console.log("You win!!!");
+      mensaje = "You win!!!";
     }else if(userSelect == "paper" && machineSelect == "scissors" || userSelect == "rock" && machineSelect == "paper" || userSelect == "scissors" && machineSelect == "rock"){
-      console.log("You Lose!!!")
+      mensaje = "You Lose!!!";
+    }else {
+      mensaje = "unidentified value, play again"
     }
   }else {
-    console.log("Tie");
+    mensaje = "Tie";
   }
-}
-resultado("paper","paper");
+  console.log(mensaje);
