2d7a96229100fab3289ae41fa9b5d141ed286a87
Nicolás Vanegas <Nicolas.vanegas.p@hotmail.com>
2020-08-08 10:12:42 -0500

    Agrego modal fijo pero con resultado equivocado

--- main.js
+++ main.js
@@ -4,26 +4,49 @@ var userSelect;/* prompt("Enter your choice between rock, papers and scissors");
 /* var weapon1 = document.getElementById("rock").addEventListener("click");
 var weapon2 = document.getElementById("paper").addEventListener("click");
 var weapon3 = document.getElementById("scissors").addEventListener("click"); */
+var userScore = 0;
+var machineSelect;
+var machineScore = 0;
 var rock = document.getElementById("rock");
 var paper = document.getElementById("paper");
 var scissors = document.getElementById("scissors");
-var machineSelect;
+const userScore_span = document.getElementById("user-score");
+const machineScore_span = document.getElementById("machine-score");
+var restart = document.getElementById("restart");
 var mensaje = document.getElementById("mensaje");
 var modal = document.querySelector(".modal");
 
 //Cuando le de click a algun boton, le asigna un valor a userSelect
-rock.addEventListener('click',function() {
+
+function selectRock() {
   userSelect = "rock";
-  play(userSelect, machineSelect);
+  play(userSelect,machineSelect);
+}
+function selectPaper() {
+  userSelect = "paper";
+  play(userSelect,machineSelect);
+}
+function selectScissors() {
+  userSelect = "scissors";
+  play(userSelect,machineSelect);
+}
+rock.addEventListener("click",selectRock);
+paper.addEventListener("click",selectPaper);
+scissors.addEventListener("click",selectScissors);
+
+/* rock.addEventListener('click',function() {
+  userSelect = "rock";
+  play(userSelect,machineSelect);
 })
 paper.addEventListener('click',function() {
   userSelect = "paper";
-  play(userSelect, machineSelect);
+  play(userSelect,machineSelect);
 })
 scissors.addEventListener('click',function() {
   userSelect = "scissors";
-  play(userSelect, machineSelect);
-})
+  play(userSelect,machineSelect);
+}) */
+
 //darle un arma aleatoria a la maquina
 var machineSelectNumber = Math.round(Math.random()* 3);
 if(machineSelectNumber == 1) {
@@ -33,30 +56,41 @@ if(machineSelectNumber == 1) {
 }else{
   var machineSelect = "scissors";
 }
-
 console.log(machineSelect); 
 console.log(userSelect); 
 
 
 //Juego
-function play(userSelect, machineSelect){
+function play(userSelect,machineSelect){
   if(userSelect != machineSelect) {
     if(userSelect == "rock" && machineSelect == "scissors" || userSelect == "scissors" && machineSelect == "paper" || userSelect == "paper" && machineSelect == "rock"){
-    mensaje.innerHTML = `<h1>You win!!!</h1><br><p>the machine chose ${machineSelect}`;
-    modal.style.display = 'block';
+      userScore++;
+      userScore_span.innerHTML = userScore;  
+      mensaje.innerHTML = `<h1>You win!!!</h1><br><p>the machine chose ${machineSelect}`;
+      modal.style.display = 'block';
     }else if(userSelect == "paper" && machineSelect == "scissors" || userSelect == "rock" && machineSelect == "paper" || userSelect == "scissors" && machineSelect == "rock"){
-    mensaje.innerHTML = `<h1>You Lose!!!</h1><br><p>the machine chose ${machineSelect}`;
-    modal.style.display = 'block';
+      machineScore++;
+      machineScore_span.innerHTML = machineScore;
+      mensaje.innerHTML = `<h1>You Lose!!!</h1><br><p>the machine chose ${machineSelect}`;
+      modal.style.display = 'block';
     }
   }else {
     mensaje.innerHTML = `<h1>tie!!!</h1><br><p>the machine chose ${machineSelect}`;
     modal.style.display = 'block';
   }
 }
+//modal
 function clearModal(e){
   if(e.target == modal) {
     modal.style.display = "none"
   }
 }
+function restartGame(){
+  userScore = 0;
+  cpuScore = 0;
+  userScore_span.innerHTML = userScore;
+  machineScore_span.innerHTML = cpuScore;
+}
+restart.addEventListener('click',restartGame);
 window.addEventListener('click', clearModal);
-play();
+play(userScore,machineScore);
