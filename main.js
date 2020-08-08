90dd6f10acb5793ee5613bb49589cf0097c138eb
Nicolás Vanegas <Nicolas.vanegas.p@hotmail.com>
2020-08-07 21:52:49 -0500

    Funciona el algoritmo con los datos del html pero mal, arreglar el algoritmo y el modal sale rápido y se va

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 //Problema: Juego de piedra papel o tijera, darle una de las 3 elecciones al usuario y a la maquina y decirles quien ganó.
 
 var userSelect;/* prompt("Enter your choice between rock, papers and scissors"); //para que ingrese la eleccion que desea. */
-var weapon1 = "rock";
-var weapon2 = "paper";
-var weapon3 = "scissors";
+/* var weapon1 = document.getElementById("rock").addEventListener("click");
+var weapon2 = document.getElementById("paper").addEventListener("click");
+var weapon3 = document.getElementById("scissors").addEventListener("click"); */
+var rock = document.getElementById("rock");
+var paper = document.getElementById("paper");
+var scissors = document.getElementById("scissors");
 var machineSelect;
-var mensaje;
+var mensaje = document.getElementById("mensaje");
+var modal = document.querySelector(".modal");
 
-
-//Para darle interacción a los iconos
-document.getElementById("rock").addEventListener("click", weapon1);
-document.getElementById("paper").addEventListener("click", weapon2);
-document.getElementById("scissors").addEventListener("click", weapon3);
-
-
-
+//Cuando le de click a algun boton, le asigna un valor a userSelect
+rock.addEventListener('click',function() {
+  userSelect = "rock";
+  play(userSelect, machineSelect);
+})
+paper.addEventListener('click',function() {
+  userSelect = "paper";
+  play(userSelect, machineSelect);
+})
+scissors.addEventListener('click',function() {
+  userSelect = "scissors";
+  play(userSelect, machineSelect);
+})
 //darle un arma aleatoria a la maquina
 var machineSelectNumber = Math.round(Math.random()* 3);
 if(machineSelectNumber == 1) {
@@ -26,22 +35,21 @@ if(machineSelectNumber == 1) {
 }
 
 console.log(machineSelect); 
+console.log(userSelect); 
 
-if(weapon1 != null || weapon1 != undefined && weapon2 != null || weapon2 != undefined){
-  if (machineSelect == "rock"){
-    mensaje = "You Lose!!!"
-  }
-}
 //Juego
-/* if(userSelect != machineSelect) {
-  if(userSelect == "rock" && machineSelect == "scissors" || userSelect == "scissors" && machineSelect == "paper" || userSelect == "paper" && machineSelect == "rock"){
-    mensaje = "You win!!!";
-  }else if(userSelect == "paper" && machineSelect == "scissors" || userSelect == "rock" && machineSelect == "paper" || userSelect == "scissors" && machineSelect == "rock"){
-    mensaje = "You Lose!!!";
+
+function play(userSelect, machineSelect){
+  if(userSelect != machineSelect) {
+    if(userSelect == "rock" && machineSelect == "scissors" || userSelect == "scissors" && machineSelect == "paper" || userSelect == "paper" && machineSelect == "rock"){
+    mensaje.innerHTML = `<h1>You win!!!</h1>`;
+    modal.style.display = 'block';
+    }else if(userSelect == "paper" && machineSelect == "scissors" || userSelect == "rock" && machineSelect == "paper" || userSelect == "scissors" && machineSelect == "rock"){
+    mensaje.innerHTML = `<h1>You Lose!!!</h1>`;
+    modal.style.display = 'block';
+    }
   }else {
-    mensaje = "unidentified value, play again"
+    mensaje.innerHTML = `<h1>tie!!!</h1>`;
+    modal.style.display = 'block';
   }
-}else {
-  mensaje = "Tie";
 }
-console.log(mensaje); */
